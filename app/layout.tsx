f9b0aafcba52d962748a0a48d9e3b22a97820ea5
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2025-01-06 09:55:57 -0800

    Remove footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Footer from "./footer";
 
 export const metadata: Metadata = {
   title: "Cloudflare Compliment Generator",
@@ -16,7 +15,6 @@ export default function RootLayout({
     <html lang="en">
       <body className="min-h-screen flex flex-col justify-between">
         {children}
-        <Footer />
       </body>
     </html>
   );
